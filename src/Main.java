14386d97ff8491a6ccb00279736402e3e2e04eb7
Bob Baker <bob@loonucorn.com>
2022-08-23 16:30:03 +0100

    GUI updated

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.awt.EventQueue;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		LoginGUIView loginGUI = new LoginGUIView();
+		DatabaseModel DB = new DatabaseModel("root","root");
+		LoginController loginController = new LoginController(loginGUI, DB);
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginGUI.getFrame().setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
